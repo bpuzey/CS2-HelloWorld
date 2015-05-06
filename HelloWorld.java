76a9a127ccab3217991b0da5b6887f21f8fdc01c
bpuzey <bpuzey661@go.tahomasd.us>
2015-05-06 08:55:34 -0700

    Added name scanner, "Random" deleted

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class HelloWorld
 {
+  private static Scanner in = new Scanner(System.in);
   public static void main(String[] args)
   {
     System.out.println("Hello World!");
-    System.out.println("Random");
+    System.out.println("Name? >");
+    String x = in.nextLine();
     System.out.println("Goodbye");
   }
 }
